6eab9da4b0627d1f16b2e8458b8c28a996742a3c
Charlie <charlesobird+github@outlook.com>
2023-05-07 13:00:37 +0100

    condense formatting into lambda functions

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from algorithmStore import getAlgorithm,storeAlgorithms,getCategories,getAlgorithms
-from time import time,sleep
-import re
+from algorithmStore import getAlgorithm,getCategories,getAlgorithms
+from time import time
 from re import search
 testData = [1,5,6,7,8,9]
 testQuery = 200
 categories = getCategories()
 debug = True
-numberCheck = lambda text: re.search("^\d*$", text)
 
-def formatSearchResult(isFound, queryIndex, query):
-    queryRes = f'{query} was found at index {queryIndex}' if isFound else f'{query} was not found'
-    return queryRes + "\n"
-    
-def formatSortResult(isSorted, sortedData, originalData):
-    sortRes = f'Original: {originalData}\nSorted: {sortedData}' if isSorted == True else 'Data was not sorted'
-    return sortRes + "\n"
+# lambda functions
+numberCheck = lambda text: search("^\d*$", text)
 
+formatEndTime = lambda end: f"\nAlgorithm finished in {round(end_time, 2)}s."
+formatSearchResult = lambda isFound, index, query: f'{query} was found at index {index}' if isFound else f'{query} was not found' + "\n"
+formatSortResult = lambda isSorted, sortedData, originalData: f'Original: {originalData}\nSorted: {sortedData}' if isSorted == True else 'Data was not sorted' + "\n"
+
+# main code
 if __name__ == "__main__":
     category = "Searching" if debug else input(f"What type of algorithm would you like to use? \nValid Inputs: {categories} \n") 
     if category in categories:
         algorithm = "Linear" if debug else input(f"What is the name of the algorithm you would you like to use? \nValid Inputs: {getAlgorithms(category)} \n")
         runAlgorithm = getAlgorithm(category, algorithm)
-        start_time = time()
-        end_time = None
+
         dataSet = input(f"Please input the data you wish to use, each item should be seperated by commas: \nExample: 1,2,3 or q,a,e,c \n")
         newDataSet = []
         for item in dataSet.split(','):
@@ -32,15 +29,16 @@ if __name__ == "__main__":
             newDataSet.append(item)
         if category == "Searching":
             query = input("What item would you like to search for? ")
-            r = runAlgorithm(newDataSet, query)
-            end_time = time() - start_time
-            isFound, queryIndex = r[0],r[1]
-            message = formatSearchResult(isFound, queryIndex, query)
+            start_time = time()
+            result = runAlgorithm(newDataSet, query)
+            end_time = start_time - time()
+            isFound, queryIndex = rresult[0],result[1]
+            message = formatSearchResult(isFound, queryIndex, query) + formatEndTime(end_time)
         elif category == "Sorting":
             order = input("What order would you like the data to be sorted into? ")
-            r = runAlgorithm(newDataSet, order)
-            end_time = time() - start_time
-            isSorted, sortedData = r[0],r[1]
-            message = formatSortResult(isSorted, sortedData, originalData)
-        print(message, f"\nAlgorithm finished in {round(end_time, 2)}s.", end="\n")
+            result = runAlgorithm(newDataSet, order)
+            end_time = start_time - time()
+            isSorted, sortedData = result[0],result[1]
+            message = formatSortResult(isSorted, sortedData, originalData) + formatEndTime(end_time)
+        print(message, end="\n")
         
