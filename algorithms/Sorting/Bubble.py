4f0ba94754cbf3bb9d44e1edefe9b2b477671cfc
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2023-05-22 15:58:16 +0100

    2/3 sorting algorithms written

--- algorithms/Sorting/Bubble.py
+++ algorithms/Sorting/Bubble.py
@@ -1,8 +1,20 @@
 # order can have two possible values, Asc (ascending) and Des (descending)
+from ...functions import typeCheck
 def run(dataSet, order):
     # sorting goes here
-    if order == "Des":
-        sortedData = dataSet.reverse()
-    else:
-        sortedData = dataSet
+    newDataSet = []
+    sortedData = []
+    for index, currentItem in dataSet.enumerate():
+        nextItem = dataSet[index + 1]
+        # only supports numbers for now
+        if typeCheck(currentItem) == True and typeCheck(nextItem) == True:
+            if currentItem > nextItem:
+                newDataSet[index] = nextItem
+                newDataSet[index + 1] = currentItem
+            else:
+                continue # no swaps made as items equal or [current < next]
+    if order == "Des": # Biggest to Smallest
+        sortedData = newDataSet.reverse()
+    else: # Smallest to Biggest
+        sortedData = newDataSet
     return [True, sortedData, False] # [Sorted, sortedData, Error]
--- algorithms/Sorting/Merge.py
+++ algorithms/Sorting/Merge.py
@@ -1,5 +1,46 @@
 # order can have two possible values, Asc (ascending) and Des (descending)
-def run(dataSet, order):
-    # sorting goes here
+from ...functions import typeCheck
+def merge(leftData, rightData):
+    mergedItems = []
+    leftLength, rightLength = len(leftData), len(rightData)
+    leftIndex, rightIndex = 0, 0
+    while leftIndex < leftLength and rightIndex < rightLength:
+        currentItemLeft = leftData[leftIndex]
+        currentItemRight = rightData[rightIndex]
+        if typeCheck(currentItemLeft) == True and typeCheck(currentItemRight) == True:
+            if currentItemLeft > currentItemRight:
+                mergedItems.append(currentItemLeft)
+                leftIndex += 1
+            elif currentItemLeft < currentItemRight:
+                mergedItems.append(currentItemRight)
+                rightIndex += 1
+    while leftIndex < leftLength:
+        mergedItems.append(leftData[leftIndex])
+        leftIndex += 1
+    while rightIndex < rightLength:
+        mergedItems.append(rightData[rightIndex])
+        rightIndex += 1
+    return mergedItems
+
+def run(dataSet, order, r=None): 
+    # dataSet = data to sort
+    # order = "Asc"ending or "Des"cending
+    # r = recursion? or final return 
     sortedData = []
-    return [True, sortedData, False] # [Sorted, sortedData, Error]
+    dataSet_length = len(dataSet)
+    midpoint = (dataSet_length - 1) // 2 # perform Floor or Integer Division
+    left_half = dataSet[0:midpoint+1] # Create left half list
+    right_half = dataSet[midpoint+1:dataSet_length] # Create right half list
+
+    left_half = run(left_half, order, True)
+    right_half = run(right_half, order, True)
+
+    mergedItems = merge(left_half,right_half)
+    if r: 
+        return
+    else:
+        if order == "Des": # Biggest to Smallest
+            sortedData = mergedItems.reverse()
+        else: # Smallest to Biggest
+            sortedData = mergedItems
+        return [True, sortedData, False] # [Sorted, sortedData, Error]
